578ede00dbba512aeef9354d7f4ea7914179e2f1
sa5ha4 <sa6ulja2008@gmail.com>
2025-11-20 13:29:32 +0200

    kods.py

---
+++ kods.py
@@ -0,0 +1,8 @@
+import random
+
+print("Kā es varu jums palīdzīdzēt?")
+
+#produkti
+produkti = ["ceptu ābolu kārums", "šokolādes müllermilch", "NESCAFE Brown sugar", "čili citruss Ādažu čipsi", "AriZona Sweet Tea Real Brewed"]
+
+atlaide = [5, 10, 15, 20]
